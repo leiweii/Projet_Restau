51de482b752ac1c54d07e8f3f7e4d74f931b4799
leiwei <shileiwei200@gmail.com>
2025-03-25 11:39:10 +0100

    fini les fonctionnalités

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from .models import Categorie
+from .models import Categorie, Plat
+from django.db.models import Q
 
 def menu_view(request):
-    categories = Categorie.objects.prefetch_related('plat_set').all()
-    return render(request, 'menu/menu.html', {'categories': categories})
+    categories = Categorie.objects.all()
+
+    # Get params
+    categorie_id = request.GET.get('categorie')
+    recherche = request.GET.get('recherche')
+    tri_prix = request.GET.get('prix')
+
+    plats = Plat.objects.filter(disponible=True)
+
+    if categorie_id and categorie_id != 'all':
+        plats = plats.filter(categorie_id=categorie_id)
+
+    if recherche:
+        plats = plats.filter(Q(nom__icontains=recherche) | Q(description__icontains=recherche))
+
+    if tri_prix == 'asc':
+        plats = plats.order_by('prix')
+    elif tri_prix == 'desc':
+        plats = plats.order_by('-prix')
+
+    return render(request, 'menu/menu.html', {
+        'categories': categories,
+        'plats': plats,
+        'categorie_id': categorie_id,
+        'recherche': recherche,
+        'tri_prix': tri_prix,
+    })
